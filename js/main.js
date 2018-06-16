1ba60b1e893472ba4c70940af9cab65318db7dd1
Anton S <asto1387@gmail.com>
2018-06-16 22:03:54 +0300

    add mail save + close modal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
     var menu = document.querySelector('#nav-menu');
-    // console.log('main menu', menu)
     menu.classList.toggle('open')
     var menuIcon = document.querySelector('.menu-resp-btn');
     (menuIcon.innerText === '☰') ? menuIcon.innerHTML = '&times;': menuIcon.innerText = '☰'
@@ -8,7 +7,9 @@ function toggleMenu() {
 
 function openModal() {
     var modalSelector = document.querySelector('.modal');
-    // console.log(modalSelector);
-
     modalSelector.classList.toggle('show-modal')
+}
+
+function saveMail() {
+    var mailAddr = document.querySelector('input.mail-sumbit').value
 }
